91b910602ec7b07e4a82af5c21119e8488311aee
dnimo <zhangmasano@icloud.com>
2021-03-19 14:31:58 +0800

    模型调试完毕，目前损失太高需要进行参数调整

--- demo01.py
+++ demo01.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
+#! -*- coding:utf-8 -*-
 
 import json
 import numpy as np
 from random import choice
-from tqdm import tqdm
 from keras_bert import load_trained_model_from_checkpoint, Tokenizer
 import re, os
 import codecs
 
 
 mode = 0
-maxlen = 160
+maxlen = 100
 learning_rate = 5e-5
 min_learning_rate = 1e-5
 
-config_path = '2021Bert/chinese_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '2021Bert/chinese_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '2021Bert/chinese_L-12_H-768_A-12/vocab.txt'
+config_path = './chinese_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = './chinese_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = './chinese_L-12_H-768_A-12/vocab.txt'
 
 
 token_dict = {}
@@ -25,6 +24,7 @@ with codecs.open(dict_path, 'r', 'utf8') as reader:
     for line in reader:
         token = line.strip()
         token_dict[token] = len(token_dict)
+
 
 # 重写tokenizer
 class OurTokenizer(Tokenizer):
@@ -43,19 +43,17 @@ tokenizer = OurTokenizer(token_dict)
 
 
 train_data = json.load(open('./data/train_data_all.json'))
-dev_data = json.load(open('./data/'))
-id2predicate, predicate2id = json.load(open('./data/all_schems_me.json'))
-id2predicate = {int(i):j for i,j in id2predicate.items()}
+dev_data = json.load(open('./data/train_data_all.json'))
+id2predicate, predicate2id = json.load(open('./data/all_schemas_me_chars.json'))
+id2predicate = {int(i): j for i, j in id2predicate.items()}
 num_classes = len(id2predicate)
-
 
 total_data = []
 total_data.extend(train_data)
 total_data.extend(dev_data)
 
-
-if not os.path.exits('./data/random_order_train_dev.json'):
-    random_order = range(len(total_data))
+if not os.path.exists('./data/random_order_train_dev.json'):
+    random_order = list(range(len(total_data)))
     np.random.shuffle(random_order)
     json.dump(
         random_order,
@@ -67,8 +65,7 @@ else:
 
 
 train_data = [total_data[j] for i, j in enumerate(random_order) if i % 8 != mode]
-dev_data = [total_data[j] for i, j in enumerate(random_order) if i % 8 != mode]
-
+dev_data = [total_data[j] for i, j in enumerate(random_order) if i % 8 == mode]
 
 predicates = {}
 
@@ -79,8 +76,7 @@ for d in train_data:
         predicates[sp[1]].append(sp)
 
 
-
-def seq_padding(X, padding = 0):
+def seq_padding(X, padding=0):
     L = [len(x) for x in X]
     ML = max(L)
     return np.array(
@@ -88,10 +84,21 @@ def seq_padding(X, padding = 0):
             np.concatenate([x, [padding] * (ML - len(x))]) if len(x) < ML else x for x in X
         ]
     )
+    # if len(X) <= 512:
+    #     return np.array(
+    #         [
+    #             np.concatenate([X, [padding] * (512-len(X))])
+    #         ]
+    #     )
+    # else:
+    #     return np.array(
+    #             X[:512]
+    #     )
+
 
 def list_find(list1, list2):
     n_list2 = len(list2)
-    for i in range(len(list1)):
+    for i in list(range(len(list1))):
         if list1[i: i+n_list2] == list2:
             return i
     return -1
@@ -108,7 +115,7 @@ class data_generator:
         return self.steps
     def __iter__(self):
         while True:
-            idxs = range(len(self.data))
+            idxs = list(range(len(self.data)))
             np.random.shuffle(idxs)
             T1, T2, S1, S2, K1, K2, O1, O2 = [], [], [], [], [], [], [], []
             for i in idxs:
@@ -121,13 +128,12 @@ class data_generator:
                     subjectid = list_find(tokens, sp[0])
                     objectid = list_find(tokens, sp[2])
                     if subjectid != -1 and objectid != -1:
-                        key = (subjectid, subjectid+len(sp[0]))
+                        key = (subjectid, subjectid + len(sp[0]))
                         if key not in items:
                             items[key] = []
                         items[key].append((objectid,
-                                          objectid+len(sp[2]),
-                                          predicate2id[sp[1]])
-                                          )
+                                           objectid + len(sp[2]),
+                                           predicate2id[sp[1]]))
                 if items:
                     t1, t2 = tokenizer.encode(first=text)
                     T1.append(t1)
@@ -135,18 +141,18 @@ class data_generator:
                     s1, s2 = np.zeros(len(tokens)), np.zeros(len(tokens))
                     for j in items:
                         s1[j[0]] = 1
-                        s2[j[1]-1] = 1
-                    k1, k2 = np.array(items.keys()).T
+                        s2[j[1] - 1] = 1
+                    k1, k2 = np.array(list(items.keys())).T
                     k1 = choice(k1)
                     k2 = choice(k2[k2 >= k1])
                     o1, o2 = np.zeros((len(tokens), num_classes)), np.zeros((len(tokens), num_classes))
                     for j in items.get((k1, k2), []):
                         o1[j[0]][j[2]] = 1
-                        o2[j[1]-1][j[2]] = 1
+                        o2[j[1] - 1][j[2]] = 1
                     S1.append(s1)
                     S2.append(s2)
                     K1.append([k1])
-                    K2.append([k2-1])
+                    K2.append([k2 - 1])
                     O1.append(o1)
                     O2.append(o2)
                     if len(T1) == self.batch_size or i == idxs[-1]:
@@ -164,8 +170,10 @@ class data_generator:
 from keras.layers import *
 from keras.models import Model
 import keras.backend as K
+import tensorflow as tf
 from keras.callbacks import Callback
 from keras.optimizers import Adam
+
 
 def seq_gather(x):
     # seq是[none,seq_len,s_size]的格式，idxs是[None,1]的格式
@@ -175,14 +183,13 @@ def seq_gather(x):
     batch_idxs = K.arange(0, K.shape(seq)[0])
     batch_idxs = K.expand_dims(batch_idxs, 1)
     idxs = K.concatenate([batch_idxs, idxs], 1)
-    return K.tf.gather_nd(seq, idxs)
+    return tf.gather_nd(seq, idxs)
 
 
 bert_model = load_trained_model_from_checkpoint(config_path, checkpoint_path, seq_len=None)
 
 for l in bert_model.layers:
     l.trainable = True
-
 
 t1_in = Input(shape=(None,))
 t2_in = Input(shape=(None,))
@@ -201,7 +208,6 @@ ps1 = Dense(1, activation='sigmoid')(t)
 ps2 = Dense(1, activation='sigmoid')(t)
 
 subject_model = Model([t1_in, t2_in], [ps1, ps2])
-
 
 k1v = Lambda(seq_gather)([t, k1])
 k2v = Lambda(seq_gather)([t, k2])
@@ -246,7 +252,7 @@ def extract_items(text_in):
         j = _k2[_k2 >= i]
         if len(j) > 0:
             j = j[0]
-            _subject = text_in[i-1: j]
+            _subject = text_in[i - 1: j]
             _subjects.append((_subject, i, j))
     if _subjects:
         R = []
@@ -254,16 +260,15 @@ def extract_items(text_in):
         _t2 = np.repeat(_t2, len(_subjects), 0)
         _k1, _k2 = np.array([_s[1:] for _s in _subjects]).T.reshape((2, -1, 1))
         _o1, _o2 = object_model.predict([_t1, _t2, _k1, _k2])
-        for i,_subject in enumerate(_subjects):
+        for i, _subject in enumerate(_subjects):
             _oo1, _oo2 = np.where(_o1[i] > 0.5), np.where(_o2[i] > 0.4)
             for _ooo1, _c1 in zip(*_oo1):
                 for _ooo2, _c2 in zip(*_oo2):
                     if _ooo1 <= _ooo2 and _c1 == _c2:
-                        _object = text_in[_ooo1-1: _ooo2]
+                        _object = text_in[_ooo1 - 1: _ooo2]
                         _predicate = id2predicate[_c1]
                         R.append((_subject[0], _predicate, _object))
                         break
-        zhuanji, gequ = [], []
         spo_list = set()
         for s, p, o in R:
             spo_list.add((s, p, o))
@@ -278,9 +283,8 @@ class Evaluate(Callback):
         self.best = 0.
         self.passed = 0
         self.stage = 0
+
     def on_batch_begin(self, batch, logs=None):
-        """第一个epoch用来warmup，第二个epoch把学习率降到最低
-        """
         if self.passed < self.params['steps']:
             lr = (self.passed + 1.) / self.params['steps'] * learning_rate
             K.set_value(self.model.optimizer.lr, lr)
@@ -290,20 +294,24 @@ class Evaluate(Callback):
             lr += min_learning_rate
             K.set_value(self.model.optimizer.lr, lr)
             self.passed += 1
+
     def on_epoch_end(self, epoch, logs=None):
         f1, precision, recall = self.evaluate()
         self.F1.append(f1)
         if f1 > self.best:
             self.best = f1
             train_model.save_weights('best_model.weights')
-        print 'f1: %.4f, precision: %.4f, recall: %.4f, best f1: %.4f\n' % (f1, precision, recall, self.best)
+        print('f1: %.4f, precision: %.4f, recall: %.4f, best f1: %.4f\n' % (f1, precision, recall, self.best))
+
     def evaluate(self):
         orders = ['subject', 'predicate', 'object']
         A, B, C = 1e-10, 1e-10, 1e-10
         F = open('dev_pred.json', 'w')
-        for d in tqdm(iter(dev_data)):
+        for d in dev_data:
             R = set(extract_items(d['text']))
-            T = set(d['spo_list'])
+            T = set()
+            for iterm in d['spo_list']:
+                T.add(tuple(iterm))
             A += len(R & T)
             B += len(R)
             C += len(T)
@@ -322,14 +330,13 @@ class Evaluate(Callback):
                     dict(zip(orders, spo)) for spo in T - R
                 ]
             }, ensure_ascii=False, indent=4)
-            F.write(s.encode('utf-8') + '\n')
+            F.write(s + '\n')
         F.close()
         return 2 * A / (B + C), A / B, A / C
 
 
 train_D = data_generator(train_data)
 evaluator = Evaluate()
-
 
 if __name__ == '__main__':
     train_model.fit_generator(train_D.__iter__(),
